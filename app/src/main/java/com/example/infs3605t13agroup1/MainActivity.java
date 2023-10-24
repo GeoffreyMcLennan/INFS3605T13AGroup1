aaec1a610e2ccb095bdc7185d2e3b8367d23c882
James Hunter z5313153 <z5313153@ad.unsw.edu.au>
2023-10-24 20:06:33 +1100

    added sos and nav bar 24/10

--- app/src/main/java/com/example/infs3605t13agroup1/MainActivity.java
+++ app/src/main/java/com/example/infs3605t13agroup1/MainActivity.java
@@ -1,14 +1,42 @@
 package com.example.infs3605t13agroup1;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
 public class MainActivity extends AppCompatActivity {
+    private RecyclerView.LayoutManager layoutManager;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        layoutManager = new LinearLayoutManager(this);
+
+        BottomNavigationView bottomNavView = findViewById(R.id.bottom_nav_view);
+        final Context context = this; // Store MainActivity.this in a final variable
+        bottomNavView.setOnNavigationItemSelectedListener(item -> {
+            if (item.getItemId() == R.id.menu_settings) {
+                startActivity(new Intent(context, SettingsActivity.class));
+                return true;
+            } else if (item.getItemId() == R.id.menu_home) {
+                startActivity(new Intent(context, MainActivity.class));
+                return true;
+            } else if (item.getItemId() == R.id.menu_profile) {
+                startActivity(new Intent(context, ProfileActivity.class));
+                return true;
+            } else {
+                return false;
+            }
+        });
+
     }
 }
---
+++ app/src/main/java/com/example/infs3605t13agroup1/ProfileActivity.java
@@ -0,0 +1,7 @@
+package com.example.infs3605t13agroup1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class ProfileActivity extends AppCompatActivity {
+
+}
