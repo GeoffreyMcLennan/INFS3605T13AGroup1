efd371b273abcfa3681792597a322fb4dbe87c72
James Hunter z5313153 <z5313153@ad.unsw.edu.au>
2023-11-03 15:38:41 +1100

    3/11

--- app/src/main/java/com/example/infs3605t13agroup1/MainActivity.java
+++ app/src/main/java/com/example/infs3605t13agroup1/MainActivity.java
@@ -63,7 +63,6 @@ public class MainActivity extends AppCompatActivity {
         }
         getCurrentLocation();
 
-
         policeButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
